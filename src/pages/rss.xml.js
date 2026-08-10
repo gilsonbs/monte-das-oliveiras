bebf101474ea35fb1c32b226edd0c31c46b5ce1c
Gilson Barreto da Silva <gilson.barreto2009@gmail.com>
2026-08-10 13:11:55 -0300

    Add enclosure to RSS feed items
    Refactor RSS feed item generation to include enclosure for social cards.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,14 +10,19 @@ export async function GET() {
     .limit(20);
 
   const site = 'https://montedasoliveiras.com';
+  const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
 
-  const items = (posts || []).map(post => `
+  const items = (posts || []).map(post => {
+    const cardUrl = `${supabaseUrl}/storage/v1/object/public/social-cards/${post.slug}.jpg`;
+    return `
     <item>
       <title><![CDATA[${post.title}]]></title>
       <link>${site}/${post.slug}</link>
       <guid isPermaLink="true">${site}/${post.slug}</guid>
       <pubDate>${new Date(post.published_at).toUTCString()}</pubDate>
-    </item>`).join('\n');
+      <enclosure url="${cardUrl}" type="image/jpeg" length="0"/>
+    </item>`;
+  }).join('\n');
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
